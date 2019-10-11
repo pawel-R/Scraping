6836a8d7a2fa46218721a488f4276cf16eac1c70
Paweł Rzepczyński <nabyleco4@wp.pl>
2019-10-11 19:28:18 +0200

    Add comments to Scraping_x-kom.py

--- Scraping_x-kom.py
+++ Scraping_x-kom.py
@@ -14,26 +14,31 @@ while (True):
 
 
     for k, v in enumerate(container):
+
+        # Get the name of the product.
         print("Product:")
         name = container[k].find("h3").string
         print(name)
 
+        # Get the rate of the product.
         rate = container[k].find(class_="sc-4el5v8-16 kIMsrT sc-1u6l9jk-1 ixvhfp sc-673ayz-0 dfQSis")["title"]
         mark = rate[-3:]
+
+        # Check if product has a rate.
         if mark[-1] == "ę":
-            mark = 0
+            print("There are no rates yet")
         else:
             mark = mark.replace(",", ".")
-            mark = float(mark)
+            print(f"Rate: {mark}")
 
-        print(f"\nRate: {mark}")
-
+        # Get the discription of the product.
         print("\nDiscription:")
         discription = container[k].find(class_="sc-1vco2i8-0 sc-4el5v8-15 fLNKyj sc-1vco2i8-1 ZosGi")
         li = discription.find_all("li")
         for i in li:
             print(i.get_text())
 
+        # Check if shipping is free.
         try:
             shipping = container[k].find(class_="sc-4el5v8-19 hqUOSY sc-1xk7jm0-2 fpNmST").get_text()
             print("\nShipping: Free")
@@ -41,6 +46,7 @@ while (True):
             shipping = False
             print("\nShipping: not free")
 
+        # Check if there is discount.
         try:
             old_Price = container[k].find(class_="apyg3s-0 apyg3s-2 iFPESx").get_text()
             print(f"Old price: {old_Price}")
@@ -56,6 +62,7 @@ while (True):
         else:
             print(f"New price: {new_Price}")
 
+        # If there is an old_price show discount.
         if bool(old_Price) == True:
             x = float(old_Price[:-3].replace(",", ".").replace(" ", ""))
             y = float(new_Price[:-3].replace(",", ".").replace(" ", ""))
@@ -65,5 +72,5 @@ while (True):
 
         print("---------------------------------------")
         print()
-
+    # Next page.
     s += 1
